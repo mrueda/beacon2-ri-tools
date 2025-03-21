8d7bd8635b26dcae51664031105e81f58ae4a2ac
Manuel Rueda <rueda.manu@gmail.com>
2025-03-21 10:59:39 +0100

    Source commit

---
+++ scripts/01_download_external_data.py
@@ -0,0 +1,26 @@
+import os, gdown
+
+# Dictionary mapping the desired output filenames to their Google Drive file IDs
+files = {
+https://drive.google.com/file/d/1g7bUAdOj5HWos03_dWsH0Y2V9EHx54Qe/view?usp=sharing
+    'data.tar.gz.md5': '1g7bUAdOj5HWos03_dWsH0Y2V9EHx54Qe',
+    'data.tar.gz.part-00': '1GOy9yUS71UP3pYhV1KZORbxA7rPDxH3a',
+    'data.tar.gz.part-01': '19vTasSHcX47qSh_VvUkfnr-UHEvfjyR6',
+    'data.tar.gz.part-02': '1HeURlpWk1CcjqckE0g7Vp_G2-rBz-Dej',
+    'data.tar.gz.part-03': '1lwx3yeeal3otHMGyEJEsl5qqvhXUw32q',
+    'data.tar.gz.part-04': '1zp9-Tl4EyXFXbyUq7MAr7g5NdmpgMt8F',
+    'data.tar.gz.part-05': '1dUTRxjKheNZ5OoSutvdxPJ8NkYbKohbc',
+    'data.tar.gz.part-06': '1-wZPfReNAmKkY9ZWNrFmqb0pwQxvhN1w'
+}
+
+def download_if_missing(filename, file_id):
+    if os.path.exists(filename):
+        print(f"{filename} already exists. Skipping download.")
+    else:
+        url = f'https://drive.google.com/uc?export=download&id={file_id}'
+        print(f"Downloading {filename}...")
+        gdown.download(url, filename, quiet=False)
+
+# Check and download files if not present
+for filename, file_id in files.items():
+    download_if_missing(filename, file_id)
